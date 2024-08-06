0710bf0d99ff4ebddcbdab1cf60a89091946d23f
samuelfreeman <samuelmanueljnr@gmail.com>
2024-08-06 11:01:53 +0000

    work done 0000

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import Bread from "@/components/Bread";
 
 
 export default function Home() {
   return (
     <main className="">
-      <h1>Hi</h1>
+
+
+
+      <div className="flex  flex-col  gap-y-10 justify-center items-center h-screen">
+      <Bread />
+
+        <h1 className="text-4xl font-bold">Welcome to Order service</h1>
+        <h2>Click on any of the links to get started!</h2>
+      </div>
     </main>
   );
 }
